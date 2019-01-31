a9182620f80ee952c2256d3a5e6178d8c1f351f6
Fujita Yuko <fujita.yuko@tis.co.jp>
2019-01-31 14:35:49 +0900

    全件検索のmethod指定

--- src/main/java/com/example/message/MessageApplication.java
+++ src/main/java/com/example/message/MessageApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,29 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MessageApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
 
-    @Autowired
-    MessageDao messageDao;
+	@Autowired
+	MessageDao messageDao;
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> Arrays.asList("なぶ", "Nab", "ナブラーク", "Nablarch").forEach(s -> {
-            Message r = new Message();
-            r.content = s;
-            messageDao.insert(r);
-        });
-    }
+	@Bean
+	CommandLineRunner runner() {
+		return args -> Arrays.asList("なぶ", "Nab", "ナブラーク", "Nablarch").forEach(s -> {
+			Message r = new Message();
+			r.content = s;
+			messageDao.insert(r);
+		});
+	}
 
-    @RequestMapping(path = "/")
-    List<Message> all() {
-        return messageDao.selectAll();
-    }
+	@RequestMapping(method = RequestMethod.GET, path = "/")
+	List<Message> all() {
+		return messageDao.selectAll();
+	}
 
-    @RequestMapping(path = "/search")
-    Message select(@RequestParam(value="id", defaultValue="1") int id) {
-    	return messageDao.selectById(id);
-    }
+	@RequestMapping(path = "/search")
+	Message select(@RequestParam(value = "id", defaultValue = "1") int id) {
+		return messageDao.selectById(id);
+	}
 }
