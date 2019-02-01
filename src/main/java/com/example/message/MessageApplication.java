3caa3cb5fead9989a6c1a5a2186312e0c235202a
Fujita Yuko <fujita.yuko@tis.co.jp>
2019-02-01 15:53:44 +0900

    idをURLから取得する

--- src/main/java/com/example/message/MessageApplication.java
+++ src/main/java/com/example/message/MessageApplication.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/message")
 public class MessageApplication {
 
 	public static void main(String[] args) {
@@ -21,31 +24,31 @@ public class MessageApplication {
 	@Autowired
 	MessageDao messageDao;
 
-	@RequestMapping(method = RequestMethod.GET, path = "/")
+	@GetMapping("/")
 	List<Message> all() {
 		return messageDao.selectAll();
 	}
 
-	@RequestMapping(method = RequestMethod.GET, path = "/search")
-	Message select(@RequestParam(value = "id", defaultValue = "1") int id) {
+	@GetMapping("/{id}")
+	Message select(@PathVariable("id") int id) {
 		return messageDao.selectById(id);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, path = "/register")
-	void insert(@RequestParam(value = "content") String content) {
+	@PostMapping("/register")
+	void insert(@RequestParam String content) {
 		Message message = new Message();
 		message.content = content;
 		messageDao.insert(message);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, path = "/delete")
-	void delete(@RequestParam(value = "id") int id) {
+	@PostMapping("/delete")
+	void delete(@RequestParam int id) {
 		Message message = messageDao.selectById(id);
 		messageDao.delete(message);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, path = "/update")
-	void update(@RequestParam int id, String content) {
+	@PostMapping("/update/{id}")
+	void update(@PathVariable int id, @RequestParam String content) {
 		Message message = messageDao.selectById(id);
 		message.content = content;
 		messageDao.update(message);
